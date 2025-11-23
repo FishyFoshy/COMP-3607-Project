3f7094a2ddf7b137ac1389f7290158ce6b8bb59a
Nishant Rogers <157635543+NishantRogers@users.noreply.github.com>
2025-11-23 19:54:38 -0400

    Added TXTReportGenerator

--- src/main/java/group20/ReportGenerationStrategy/DOCXReportGenerator.java
+++ src/main/java/group20/ReportGenerationStrategy/DOCXReportGenerator.java
@@ -3,6 +3,7 @@ package group20.ReportGenerationStrategy;
 import group20.GameLogic.GameState;
 
 public class DOCXReportGenerator implements ReportGenerator {
+    @Override
     public void generateReport(GameState gameState, int gameId){
         
     }
